36ce492f440fb6debe2000cec756177f6cca3fba
jo0132 <noji70770@gmail.com>
2023-06-22 16:12:19 +0900

    20203.06.22

--- src/assets/js/start.js
+++ src/assets/js/start.js
@@ -59,7 +59,7 @@ ani01.to('.img_wrap .i3 img', {
 ani01
     .to('.img_wrap .i2 img', {
         // scale: 10,
-        translateX: 700,
+        translateX: 600,
     })
     .to('.img_wrap .i2 img', {
         scale: 6,
@@ -74,77 +74,22 @@ gsap.to('#section01', {
     y: '#section02',
 });
 
-function goToSession1() {
-    // 세션1의 ID를 사용하여 스크롤 이동
-    gsap.to(window, {
-        duration: 1,
-        scrollTo: {
-            y: '#section01',
-        },
-    });
-}
-
-gsap.to(window, { scrollTo: '#section01' });
-
-// 페이지 로드 시 실행되는 함수
-window.addEventListener('load', function () {
-    goToSession1();
-});
-
 // ScrollTrigger 플러그인 초기화
 gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.create({
-    trigger: '#session01',
-    start: 'top top',
-    end: 'bottom bottom',
-    onUpdate: self => {
-        self.scrollTrigger.disable(); // 스크롤 잠금
-    },
-});
-
-// section02 스크롤 트리거 정의
-ScrollTrigger.create({
-    trigger: '#section02',
-    start: 'top 50%', // 트리거 지점 설정
-    end: 'bottom top', // 애니메이션이 실행될 범위 설정
-    markers: true,
-    onEnter: () => {
-        // section02 진입 시 실행되는 코드
-        console.log('Enter section02');
-    },
-    onLeaveBack: () => {
-        // section02에서 뒤로 벗어날 때 실행되는 코드
-        console.log('Leave section02');
-        // section01로 스크롤 전환 애니메이션 실행
-        gsap.to(window, {
-            duration: 1,
-            scrollTo: {
-                y: '#section01',
-            },
-
-            onComplete: () => {
-                console.log('Scroll to section01 complete');
-                // section01 진입 후 실행할 코드
-            },
-        });
-    },
-});
 const ani02_1 = gsap.timeline();
-ani02_1.to('#Section02 .right', { y: -400, opacity: 0 }).to('#Section02 .right', {
-    y: 0,
-    opacity: 1,
-});
+ani02_1.from('#section2 > .left', { opacity: 1 }, 'a');
 
 ScrollTrigger.create({
     animation: ani02_1,
     trigger: '#section2',
-    start: 'top',
-    end: '+=1000',
+    start: 'top top',
+    end: '+=3000',
     scrub: true,
-    pin: true,
-    anticipatepin: 1,
-    markers: false,
+    pin: '#section2 > .left',
+    anticipatePin: 1,
+    pinSpacing: false, // 추가된 부분: pin 요소와 다른 컨텐츠 사이의 간격을 없앰
+    markers: true,
 });
 
 function scroll() {
