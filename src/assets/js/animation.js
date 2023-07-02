6c7fb217c8c68dcb3802cbf58c955c8526f3084f
jo0132 <noji70770@gmail.com>
2023-07-02 12:54:04 +0900

    2023.07.02

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,180 +1,217 @@
-let scrollTop = window.pageYOffset;
+let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
 const spanText = Array.from(document.getElementsByClassName('text'));
+const tlImg = Array.from(document.getElementsByClassName('tl_img'));
+const tlSpan = Array.from(document.getElementsByClassName('tl_span'));
 const sectionBg01 = document.getElementById('section01');
 const sectionBg02 = document.getElementById('section02');
 const sectionBg03 = document.getElementById('section03');
 const sectionBg04 = document.getElementById('section04');
 // const sectionBg05 = document.getElementById('section05');
-const sectionBg06 = document.getElementById('section06');
+// const sectionBg06 = document.getElementById('section06');
 const sectionBg07 = document.getElementById('section07');
-const ani01 = gsap.timeline();
 
-ani01.to(sectionBg01, {
+const ani01 = gsap.timeline({
+    onComplete: function () {
+        const ani01_end = gsap.timeline();
+        gsap.set(sectionBg01, {
+            position: 'static',
+            zIndex: 999999,
+        });
+        ani01_end
+            .to(spanText, {
+                duration: 2,
+                translateY: 1500,
+                rotate: 50,
+                stagger: {
+                    amount: 3,
+                    from: 'random',
+                },
+            })
+            .to(tlImg, {
+                duration: 2,
+                translateY: 3000,
+                rotate: 50,
+                stagger: {
+                    amount: 3,
+                    from: 'random',
+                },
+            })
+            .to(sectionBg02, { opacity: 1 })
+            .to(tlSpan, {
+                duration: 2,
+                translateY: 3000,
+                rotate: 50,
+                stagger: {
+                    amount: 3,
+                    from: 'random',
+                },
+            })
+            .to(sectionBg02, {
+                opacity: 1,
+            })
+            .to(sectionBg01, { display: 'none' });
+
+        ScrollTrigger.create({
+            animation: ani01_end,
+            trigger: sectionBg01,
+            start: 'top top',
+            end: '+=3000',
+            scrub: true,
+            pin: true,
+            pinSpacing: false,
+            markers: false,
+        });
+    },
+});
+gsap.set(sectionBg01, {
     position: 'fixed',
-    zIndex: 99999,
+    zIndex: 999999,
 });
-
-gsap.set(sectionBg02, {
+ani01.to(sectionBg02, {
     opacity: 0,
 });
-
-gsap.set(spanText, {
-    y: '-500px',
-    opacity: 0,
-    color: '#fff',
-});
-
-ani01.to(spanText, {
+ani01.from(spanText, {
+    autoAlpha: 0,
+    y: -100,
+    rotation: 360,
+    ease: 'back.out(4)',
     duration: 1,
-    y: 0,
-    rotation: 1080,
-    opacity: 1,
     stagger: {
         amount: 3,
         from: 'random',
     },
 });
-
 gsap.to(sectionBg01, {
     delay: 0.3,
     duration: 0.5,
     backgroundColor: '#000',
 });
-
-// intro span
-ani01.to('.img_wrap .i1', {
-    display: 'block',
-    rotation: 345,
-});
-ani01.to('.img_wrap .i2', {
-    display: 'block',
-    rotation: 360,
-});
-ani01.to('.img_wrap .i3', {
-    display: 'block',
-    rotation: 366.16,
-});
-
-// intro img
-ani01.to('.img_wrap .i1 img', {
-    display: 'block',
-    rotation: 360,
-});
-ani01.to('.img_wrap .i2 img', {
-    display: 'block',
-    rotation: 360,
-});
-ani01.to('.img_wrap .i3 img', {
-    display: 'block',
-    rotation: 360,
-});
-
-// 인트로 마무리
 ani01
-    .to('.img_wrap .i2 img', {
-        // scale: 10,
-        delay: 1,
-        translateX: '29vw',
+    .to(sectionBg02, { opacity: 0 })
+    .to('.img_wrap .i1', {
+        display: 'block',
+        translateX: 0,
+    })
+    .to('.img_wrap .i2', {
+        display: 'block',
+        translateX: 0,
+    })
+    .to('.img_wrap .i3', {
+        display: 'block',
+        translateX: 0,
+    })
+    .to('.img_wrap .i1 img', {
+        display: 'block',
+        scale: 0.9,
     })
     .to('.img_wrap .i2 img', {
-        duration: 0.5,
-        scale: 6,
+        display: 'block',
+        scale: 0.9,
+    })
+
+    .to('.img_wrap .i3 img', {
+        display: 'block',
+        scale: 0.9,
     });
 
-ani01.to(sectionBg02, {
-    opacity: 1,
-});
-ani01.to('#section01', {
-    delay: -0.5,
-    duration: 0.1,
-    display: 'none',
-    opacity: 0,
-    ease: 'sine.out',
-});
-
-gsap.to('#section01', {
-    y: '#section02',
-});
-
-// 페이지 로드 시 실행되는 함수
-
-// ScrollTrigger 플러그인 초기화
-gsap.registerPlugin(ScrollTrigger);
-
+//section01
 const ani02 = gsap.timeline();
-ani02
-    .from('#section02 .right', { opacity: 1, y: 0 })
-    .to('#section02 .right', { opacity: 1, y: -350 })
-    .from('#section02 .left', { opacity: 1 })
-    .to('#section02 .right', { opacity: 0, y: -700 })
-    .to('#section02 .left', { opacity: 0 });
+ani02.from('#section02 .right', { opacity: 1, y: 0 }).to('#section02 .right', { opacity: 0, y: -200 });
+ani02.from('#section02 .left', { opacity: 1 }, '<').to('#section02 .left', { opacity: 0 }, '<');
 
 ScrollTrigger.create({
     animation: ani02,
     trigger: sectionBg02,
     start: 'top top',
-    end: 'bottom top',
+    end: '+=11000',
     scrub: true,
     pin: true,
     anticipatePin: 1,
-    markers: true,
+    markers: false,
 });
+
+// const panel03 = document.querySelector('#section03');
+
+// ScrollTrigger.create({
+//     trigger: panel03,
+//     start: 'top top',
+//     end: '+=3000',
+//     pin: true,
+//     pinSpacing: false,
+// });
 
 const ani03 = gsap.timeline();
 
 ani03
-
     .from('.section03_main_desc', { opacity: 1, y: 0 })
-    .from('.section03_main_item', { opacity: 1 })
-    .to('.section03_main_desc', { opacity: 1, y: -250 })
-    .to('.section03_main_item', { opacity: 0 })
-    .to('.section03_main_desc', { opacity: 0, y: -500 })
+    .from('.section03_main_item', { opacity: 1 }, '<')
+    .to('.section03_main_item', { duration: 3, opacity: 0 }, '<')
+    .to('.section03_main_desc', { duration: 3, opacity: 0, y: -500 }, '<')
     .to(sectionBg03, { opacity: 0 });
 
 ScrollTrigger.create({
     animation: ani03,
     trigger: sectionBg03,
     start: 'top top',
-    end: 'bottom top',
+    end: '+=10000',
     scrub: true,
     pin: true,
     anticipatePin: 1,
-    markers: true,
+    markers: false,
 });
+// 세션3
 
-// const sest = document.getElementById('section01');
-const ani04 = gsap.timeline({});
+const ani04 = gsap.timeline();
 
 const titleSpan = document.querySelectorAll('.title_span');
 
 ani04
+    .from(sectionBg04, { opacity: 0 }) // 시작 상태 정의
     .to('.s4_t1', { y: '-33.5vw' })
+    .from(titleSpan, { opacity: 0 }, '<')
     .to('.s4_t2', { y: '-33.5vw' })
+    .to(titleSpan, { opacity: 1, marginRight: 0 }, '<')
     .to('.s4_t3', { y: '-33.5vw' })
-    .from(titleSpan, { opacity: 0 })
-    .to(titleSpan, { opacity: 1, marginRight: 0 });
+    .to('.section04_main_title', { scale: 10 })
+    .to(sectionBg04, { backgroundColor: '#000' }, '<')
+    .to('#section04 > div', { display: 'none' }, '<');
 
 ScrollTrigger.create({
     animation: ani04,
     trigger: sectionBg04,
     start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    pin: true,
-    anticipatePin: 1,
-    toggleClass: 'active',
-    markers: true,
+    end: '+=200%', // 고정된 상태를 유지할 스크롤 범위를 설정합니다.
+    pin: true, // 요소를 고정시킵니다.
+    markers: false,
+    onEnter: () => {
+        // 스크롤이 해당 섹션으로 다시 들어왔을 때 애니메이션 재실행
+        ani04.restart();
+    },
 });
-const sectionBg05 = document.getElementById('section05');
-document.addEventListener(sectionBg05, () => {
+
+//세션4
+
+document.addEventListener('DOMContentLoaded', () => {
     gsap.registerPlugin(ScrollTrigger);
 
     function animate(item) {
         let delay = item.dataset.delay;
         let x = 0;
         let y = 0;
-        if (item.classList.contains('BTT')) {
+        if (item.classList.contains('section05-1')) {
+            x = 0;
+            y = 100;
+        } else if (item.classList.contains('section05-2')) {
+            x = 0;
+            y = 100;
+        } else if (item.classList.contains('section05-3')) {
+            x = 0;
+            y = 100;
+        } else if (item.classList.contains('section05-4')) {
+            x = 0;
+            y = 100;
+        } else if (item.classList.contains('section05-5')) {
             x = 0;
             y = 100;
         }
@@ -182,22 +219,171 @@ document.addEventListener(sectionBg05, () => {
         gsap.fromTo(
             item,
             { autoAlpha: 0, x: x, y: y },
-            { autoAlpha: 1, x: 0, y: 0, delay: delay, duration: 1.25, overwrite: 'auto', ease: 'expo' },
+            {
+                autoAlpha: 1,
+                x: 0,
+                y: 0,
+                delay: delay,
+                duration: 1.25,
+                overwrite: 'auto',
+                ease: 'expo',
+            },
         );
     }
 
-    console.log(item);
     gsap.utils.toArray('.BTT').forEach(item => {
         ScrollTrigger.create({
             trigger: item,
-            start: 'top top',
-            markers: true,
+            start: 'top 80%',
+            markers: false,
             onEnter: () => {
                 animate(item);
             },
         });
         item.style.opacity = '0';
     });
+});
+// 세션5
+
+const sectionBg06 = document.getElementById('section06');
+const sectionItem = gsap.utils.toArray('#section06 > div');
+
+let scrollTween = gsap.to(sectionItem, {
+    xPercent: -100 * (sectionItem.length - 1),
+    ease: 'none',
+    scrollTrigger: {
+        trigger: sectionBg06,
+        start: 'top top',
+        end: () => '+=' + (sectionBg06.offsetWidth - innerWidth),
+        pin: true,
+        anticipatepin: 1,
+        scrub: 1,
+        // snap: 1 / (section.length -1),
+        snap: {
+            snapTo: 1 / (sectionItem.length - 1),
+            inertia: false,
+            duration: { min: 0.1, max: 0.1 },
+        },
+        invalidateOnRefresh: true,
+        markers: true,
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-00',
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+
+        markers: true,
+        onEnter: () => {
+            // .section06-00 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.set('.section06-00 > .section06_main > img', { width: 0 });
+            gsap.from('.section06-00 > .section06_main > h1', { duration: 1, opacity: 0, x: 400 });
+            gsap.to('.section06-00 > .section06_main > img', { duration: 1, width: '60vw' });
+            gsap.from('.section06-00 > .section06_main > p', { duration: 1, opacity: 0, x: 400 });
+        },
+    },
+});
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-01',
+        containerAnimation: scrollTween,
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+        markers: true,
+
+        onEnter: () => {
+            // .section06-01 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.from('.section06-01 > h1', { duration: 1, opacity: 0, x: 200 });
+            gsap.from('.section06-01 > .row_desc', { duration: 1, opacity: 0, x: -400 });
+        },
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-02',
+        containerAnimation: scrollTween,
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+        markers: false,
+
+        onEnter: () => {
+            // .section06-02 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.from('.section06-02 > h1', { duration: 1, opacity: 0, x: 200 });
+            gsap.from('.section06-02 > .row_desc', { duration: 1, opacity: 0, x: -400 });
+        },
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-03',
+        containerAnimation: scrollTween,
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+        markers: false,
+
+        onEnter: () => {
+            // .section06-03 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.from('.section06-03 > h1', { duration: 1, opacity: 0, x: 200 });
+            gsap.from('.section06-03 > .row_desc', { duration: 1, opacity: 0, x: -400 });
+        },
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-04',
+        containerAnimation: scrollTween,
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+        markers: false,
+
+        onEnter: () => {
+            // .section06-04 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.from('.section06-04 > h1', { duration: 1, opacity: 0, x: 200 });
+            gsap.from('.section06-04 > .row_desc', { duration: 1, opacity: 0, x: -400 });
+        },
+    },
+});
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.section06-05',
+        containerAnimation: scrollTween,
+        start: 'left center',
+        end: 'right center',
+        toggleActions: 'play none none reverse',
+        markers: false,
+
+        onEnter: () => {
+            // .section06-05 트리거에 도착했을 때 실행할 애니메이션 코드
+            gsap.from('.section06-05 > h1', { duration: 1, opacity: 0, x: 200 });
+            gsap.from('.section06-05 > .row_desc', { duration: 1, opacity: 0, x: -400 });
+        },
+    },
+});
+
+const ani07 = gsap.timeline();
+
+ani07
+    .set('.section07_img', { opacity: 0 })
+    .set('.section07_inner > h2', { duration: 1, opacity: 0, x: 400 })
+    .to('.section07_img', { duration: 1, opacity: 1 })
+    .to('.section07_inner > h2', { duration: 1, opacity: 1, x: 0 });
+
+ScrollTrigger.create({
+    animation: ani07,
+    trigger: sectionBg07,
+    start: 'top top',
+    end: '+=5000',
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    markers: false,
 });
 
 function scroll() {
